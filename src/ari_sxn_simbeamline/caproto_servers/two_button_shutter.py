756acccfaf0388b94f3dab757f063f69f6516820
Andrew Walter <awalter@bnl.gov>
2024-06-28 13:14:16 -0400

    complete the TwoButtonShutter class

--- src/ari_sxn_simbeamline/caproto_servers/two_button_shutter.py
+++ src/ari_sxn_simbeamline/caproto_servers/two_button_shutter.py
@@ -1,6 +1,7 @@
 from caproto import ChannelType
 from caproto.server import pvproperty, ioc_arg_parser, run, PVGroup
 from textwrap import dedent
+import time
 
 
 class TwoButtonShutter(PVGroup):
@@ -23,6 +24,25 @@ class TwoButtonShutter(PVGroup):
 
     """
 
+    def __init__(self, *args, actuation_time=3, **kwargs):
+        """
+        An init function that sets an attribute called _activation_time.
+
+        This performs the parent initialization and then adds abn attribute for
+        activation time that can be set during instantiation.
+
+        Parameters
+        ----------
+        *args :
+            Arguments passed to the parent class __init__ method
+        actuation_time_time :
+            The time (in seconds) that the device should take to actuate
+        **kwargs :
+           Keyword arguments passed to the parent class __init__ method
+        """
+        super().__init__(*args, **kwargs)
+        self._actuation_time = actuation_time
+
     # Shutter properties
     open_cmd = pvproperty(name=':Cmd:Opn-Cmd', dtype=ChannelType.ENUM,
                           value='Idle', enum_strings=['Idle', 'Open'])
@@ -36,6 +56,38 @@ class TwoButtonShutter(PVGroup):
                               read_only=True, report_as_string=True)
     enable_status = pvproperty(name=':Enbl-Sts', value='True',
                                read_only=True, report_as_string=True)
+
+    # noinspection PyMethodParameters
+    @open_cmd.putter
+    async def open_cmd(obj, instance, value):
+        """
+        This is a putter function that steps through the proces required when
+        the 'open_cmd' PV is set to 'Open'. If it is set to 'Not Open' it just
+        sets itself to 'Idle'.
+        """
+        if value == 'Open':
+            await instance.write('Open', verify_value=False)
+            start_timestamp = time.time()  # record initial time
+            while time.time() - start_timestamp < obj._actuation_time:
+                time.sleep(1E-3)
+
+        return 'Idle'
+
+    # noinspection PyMethodParameters
+    @close_cmd.putter
+    async def close_cmd(obj, instance, value):
+        """
+        This is a putter function that steps through the proces required when
+        the 'open_cmd' PV is set to 'Open'. If it is set to 'Not Open' it just
+        sets itself to 'Idle'.
+        """
+        if value == 'Close':
+            await instance.write('Close', verify_value=False)
+            start_timestamp = time.time()  # record initial time
+            while time.time() - start_timestamp < obj._actuation_time:
+                time.sleep(1E-3)
+
+        return 'Idle'
 
 
 # Add some code to start a version of the server if this file is 'run'.
